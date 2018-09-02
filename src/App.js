e6f3fb870a8e398a8a6268baaf49e2045e553d7f
Illya Artemchuk <ilyalinkis@gmail.com>
2018-09-02 10:34:45 -0700

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         <header>
+        <nav>
+          <Link to="/">  Landing  </Link>
+          <Link to="/library">  Library  </Link>
           <h1> Bloc Jams </h1>
+        </nav>
         </header>
       <main>
         <Route exact path="/" component={Landing} />
