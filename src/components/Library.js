dd42ec3bdcb80f16f8860eb678566d5b6d429553
Illya Artemchuk <ilyalinkis@gmail.com>
2018-09-02 11:06:07 -0700

    Added album.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     albums: albumData
+    }
+  }
   render () {
     return (
     <section className="library">
     Library will go here
     </section>
-  );
+    );
   }
 }
 
